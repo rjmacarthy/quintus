dc5e6a3bd5c6fcb3b74ae86eb5e2d6ce3f22b735
rjmacarthy <richardmacarthy@hotmail.com>
2023-05-11 15:37:47 +0100

    rename

--- lib/examples/zendesk.py
+++
@@ -1,8 +0,0 @@
-from quintus import Quintus, LoaderType
-
-quintus = Quintus()
-
-quintus.load(
-    LoaderType.ZENDESK,
-    "https://postman.zendesk.com/api/v2/help_center/en-us/articles.json",
-)
