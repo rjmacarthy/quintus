99728d3cce42aba60c25f532cfce55a1074a240e
rjmacarthy <richardmacarthy@hotmail.com>
2023-05-17 19:18:50 +0100

    add json cleaner prompt

--- lib/prompts/templates.py
+++ lib/prompts/templates.py
@@ -60,3 +60,41 @@ def get_classification_prompt(document, options, examples=[]):
         
         \n\n### Response:\n
       """
+      
+def json_cleaner_system_prompt():
+    return """      
+        You are a JSON generation agent whose sole purpose is to generate JSON from given documents, regardless of their format.
+        You will return Objects not Arrays when possible.
+        Your role is to respond with JSON in a markdown window, without providing any explanations or questions. 
+        The JSON you provide must be parseable, and if the document contains HTML, it should be removed. 
+        You might be given some arbitary data along with the data to help you generate the JSON.
+        You will receive requests for JSON generation along with the structure to generate, 
+        and your task is to respond with the JSON corresponding to the given request.
+        If the response will be too long return { error : "response too long" }
+
+        For example, if you receive the following request:
+
+        Data: "joe bloggs, male, 38"
+
+        You would reply with:
+
+        Structure: { "name": "string", "gender": "string", "age": "number" }
+
+        Output:
+        
+        {
+            "name": "Joe Bloggs",
+            "gender": "Male",
+            "age": 38
+        }
+        
+        Provide your first instruction for JSON generation. By returning hello world.
+    """.strip()
+    
+def json_cleaner_prompt(data, structure):
+    return """
+        You are a json cleaner agent. 
+        The only thing you do is clean json from given documents, the document could be anything and badly formatted.
+        return json for the given data, structure and output.
+        Data: {data}
+    """.split()
